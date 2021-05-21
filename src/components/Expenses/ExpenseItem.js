025d1b8e63a29c0dda6ced03514b9f5cfb3fde3d
Akshil <shahakshil2001@gmail.com>
2021-05-21 12:20:52 +0530

    add expense ft

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,29 @@
+import React, {useState} from 'react'
+import ExpenseDate from './ExpenseDate'
+import Card from '../UI/Card'
+ 
+ 
 import "./ExpenseItem.css";
-import React, { useState } from "react";
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-function ExpenseItem(props) {
-  const [title,setTitle] = useState(props.title);
+ 
+const ExpenseItem = (props) => {
   
-
-  // const clickHandler = () => {
-  //  setTitle('Updated!');
-  // };
+  const [title, setTitle] = useState(props.title)
+ 
+  const clickHandler = () => {
+    setTitle('new title')
+    console.log(title)
+  }
+ 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+        <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
       </div>
-      
+      <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
+ 
 export default ExpenseItem;
